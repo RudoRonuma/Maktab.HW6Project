08b248eebcb566f98d045c8dde0d342f95a2faed
Aliwoto <woto@kaizoku.cyou>
2023-01-31 04:53:08 +0330

    Add project files.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

---
+++ HW6Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW6Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
